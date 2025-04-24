1bd27fd28f5244e60a8d77028d1ee44622a515f4
panxingfeng <1115005803@qq.com>
2025-04-25 03:02:19 +0800

    chat_ui

--- chat_ui/src/components/input/ToolsPanel.js
+++ chat_ui/src/components/input/ToolsPanel.js
@@ -447,12 +447,12 @@ function ToolsPanel({ onClose, onSelectTool }) {
                 </svg>
               </button>
             )}
-            <button onClick={onClose} className="close-button" title="关闭">
+            {/* <button onClick={onClose} className="close-button" title="关闭">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <line x1="18" y1="6" x2="6" y2="18"></line>
                 <line x1="6" y1="6" x2="18" y2="18"></line>
               </svg>
-            </button>
+            </button> */}
           </div>
         </div>
         <div className="tools-panel-content">
